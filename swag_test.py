9005a484b97fb2e89f36094a3dfdf00c4d201bf9
Maria Clara Pereira de Souza <mariaclara-pereira@outlook.com>
2025-05-24 12:24:03 -0300

    modularização

--- swag_test.py
+++ swag_test.py
@@ -3,20 +3,17 @@ import json
 from selenium.webdriver.common.by import By
 
 class SwagLabsTest(BaseCase):
-    def test_fluxo_completo(self):
-        """Teste completo do fluxo do Swag Labs"""
-        # Configurações para evitar problemas
-        self.DISABLE_LOGGING = True
-        self.headless = False  # Altere para True se não quiser ver o navegador
-        
-        # 1. Login
+
+    def login(self):
+        """Acessa o site Swag Labs"""
         self.open("https://www.saucedemo.com/")
         self.type("#user-name", "standard_user")
         self.type("#password", "secret_sauce")
         self.click("#login-button")
-        self.assert_element(".inventory_list")
+        self.wait_for_element(".inventory_list")
         
-        # 2. Extração de dados dos produtos
+    def extrair_dados(self):
+        """Extrai os dados dos produtos e salva em JSON"""
         produtos = []
         items = self.find_elements(".inventory_item")
         
@@ -34,21 +31,25 @@ class SwagLabsTest(BaseCase):
         with open('produtos.json', 'w', encoding='utf-8') as f:
             json.dump(produtos, f, indent=4, ensure_ascii=False)
         
-        # 3. Adicionar itens ao carrinho
+    def adicionar_ao_carrinho(self):
+        """Adiciona todos os produtos ao carrinho"""
         add_buttons = self.find_elements("button.btn_inventory")
         for btn in add_buttons:
             btn.click()
         
-        # 4. Finalizar compra
+    def finalizar_compra(self):
+        """Termina o processo de compra"""
         self.click(".shopping_cart_link")
+        self.wait_for_element("#checkout")
         self.click("#checkout")
         
         self.type("#first-name", "Trainee")
         self.type("#last-name", "PiJunior")
         self.type("#postal-code", "31270-901")
         self.click("#continue")
+        self.wait_for_element("#finish")
         
-        # Validações finais - SELEtores ATUALIZADOS
+        # Validações finais - seletores atualizados
         payment_info = self.get_text(".summary_info > div:nth-of-type(1)")
         shipping_info = self.get_text(".summary_info > div:nth-of-type(2)")
         total = self.get_text(".summary_total_label")
@@ -61,5 +62,17 @@ class SwagLabsTest(BaseCase):
         self.click("#finish")
         
         # Verificação final
+        self.wait_for_element(".complete-header")
         self.assert_exact_text("Thank you for your order!", ".complete-header")
         print("\nTeste concluído com sucesso!")
+
+    def test_completo(self):
+        """Teste completo do fluxo do Swag Labs"""
+        # Configurações para evitar problemas
+        self.DISABLE_LOGGING = True
+        self.headless = False
+
+        self.login()
+        self.extrair_dados()
+        self.adicionar_ao_carrinho()
+        self.finalizar_compra()
